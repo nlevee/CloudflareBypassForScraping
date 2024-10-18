923509a9b265646764bf5a882d6b990eb1156339
Nicolas Levée <nicolasl@padok.fr>
2024-10-18 23:51:08 +0200

    feat: add compatibility for Jackett

--- Dockerfile
+++ Dockerfile
@@ -8,29 +8,29 @@ ENV DOCKERMODE=true
 # Install necessary packages for Xvfb and pyvirtualdisplay
 RUN apt-get update && \
     apt-get install -y \
-        python3 \
-        python3-pip \
-        wget \
-        gnupg \
-        ca-certificates \
-        libx11-xcb1 \
-        libxcomposite1 \
-        libxdamage1 \
-        libxrandr2 \
-        libxss1 \
-        libxtst6 \
-        libnss3 \
-        libatk-bridge2.0-0 \
-        libgtk-3-0 \
-        x11-apps \
-        fonts-liberation \
-        libappindicator3-1 \
-        libu2f-udev \
-        libvulkan1 \
-        libdrm2 \
-        xdg-utils \
-        xvfb \
-        && rm -rf /var/lib/apt/lists/*
+    python3 \
+    python3-pip \
+    wget \
+    gnupg \
+    ca-certificates \
+    libx11-xcb1 \
+    libxcomposite1 \
+    libxdamage1 \
+    libxrandr2 \
+    libxss1 \
+    libxtst6 \
+    libnss3 \
+    libatk-bridge2.0-0 \
+    libgtk-3-0 \
+    x11-apps \
+    fonts-liberation \
+    libappindicator3-1 \
+    libu2f-udev \
+    libvulkan1 \
+    libdrm2 \
+    xdg-utils \
+    xvfb \
+    && rm -rf /var/lib/apt/lists/*
 
 # Add Google Chrome repository and install Google Chrome
 RUN wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - && \
--- server.py
+++ server.py
@@ -10,36 +10,45 @@ from pydantic import BaseModel
 from typing import Dict
 import argparse
 
-# Check if running in Docker mode
-DOCKER_MODE = os.getenv("DOCKERMODE", "false").lower() == "true"
-
-# Chromium options arguments
-arguments = [
-    # "--remote-debugging-port=9222",  # Add this line for remote debugging
-    "-no-first-run",
-    "-force-color-profile=srgb",
-    "-metrics-recording-only",
-    "-password-store=basic",
-    "-use-mock-keychain",
-    "-export-tagged-pdf",
-    "-no-default-browser-check",
-    "-disable-background-mode",
-    "-enable-features=NetworkService,NetworkServiceInProcess,LoadCryptoTokenExtension,PermuteTLSExtensions",
-    "-disable-features=FlashDeprecationWarning,EnablePasswordsAccountStorage",
-    "-deny-permission-prompts",
-    "-disable-gpu",
-    "-accept-lang=en-US",
-    #"-incognito" # You can add this line to open the browser in incognito mode by default 
-]
-
 browser_path = "/usr/bin/google-chrome"
 app = FastAPI()
-
+log = True
 
 # Pydantic model for the response
-class CookieResponse(BaseModel):
-    cookies: Dict[str, str]
-    user_agent: str
+class V1RequestBase(BaseModel):
+    # V1RequestBase
+    cmd: str = None
+    cookies: list = None
+    maxTimeout: int = None
+    proxy: dict = None
+    session: str = None
+    session_ttl_minutes: int = None
+
+    # V1Request
+    url: str = None
+    postData: str = None
+    returnOnlyCookies: bool = None
+
+class ChallengeResolutionResultT(BaseModel):
+    url: str = None
+    status: int = None
+    headers: dict = None
+    response: str = None
+    cookies: list = None
+    userAgent: str = None
+        
+class V1ResponseBase(BaseModel):
+    # V1ResponseBase
+    status: str = None
+    message: str = ""
+    session: str = None
+    sessions: list[str] = None
+    startTimestamp: int = None
+    endTimestamp: int = None
+    version: str = "1.0.0"
+
+    # V1ResponseSolution
+    solution: ChallengeResolutionResultT = None
 
 
 # Function to check if the URL is safe
@@ -58,68 +67,55 @@ def is_safe_url(url: str) -> bool:
 def bypass_cloudflare(url: str, retries: int, log: bool) -> ChromiumPage:
     from pyvirtualdisplay import Display
 
-    if DOCKER_MODE:
-        # Start Xvfb for Docker
-        display = Display(visible=0, size=(1920, 1080))
-        display.start()
+    # Start Xvfb for Docker
+    display = Display(visible=0, size=(1920, 1080))
+    display.start()
 
-        options = ChromiumOptions()
-        options.set_argument("--auto-open-devtools-for-tabs", "true")
-        options.set_argument("--remote-debugging-port=9222")
-        options.set_argument("--no-sandbox")  # Necessary for Docker
-        options.set_argument("--disable-gpu")  # Optional, helps in some cases
-        options.set_paths(browser_path=browser_path).headless(False)
-    else:
-        options = ChromiumOptions()
-        options.set_argument("--auto-open-devtools-for-tabs", "true")
-        options.set_paths(browser_path=browser_path).headless(False)
+    options = ChromiumOptions()
+    options.set_argument("--no-sandbox")  # Necessary for Docker
+    options.set_argument("--disable-gpu")  # Optional, helps in some cases
+    options.set_paths(browser_path=browser_path).headless(False)
 
     driver = ChromiumPage(addr_or_opts=options)
     try:
+        driver.listen.start(targets=url)
         driver.get(url)
         cf_bypasser = CloudflareBypasser(driver, retries, log)
         cf_bypasser.bypass()
         return driver
     except Exception as e:
         driver.quit()
-        if DOCKER_MODE:
-            display.stop()  # Stop Xvfb
+        display.stop()  # Stop Xvfb
         raise e
 
-
-# Endpoint to get cookies
-@app.get("/cookies", response_model=CookieResponse)
-async def get_cookies(url: str, retries: int = 5):
-    if not is_safe_url(url):
-        raise HTTPException(status_code=400, detail="Invalid URL")
-    try:
-        driver = bypass_cloudflare(url, retries, log)
-        cookies = driver.cookies(as_dict=True)
-        user_agent = driver.user_agent
-        driver.quit()
-        return CookieResponse(cookies=cookies, user_agent=user_agent)
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
-
-
 # Endpoint to get HTML content and cookies
-@app.get("/html")
-async def get_html(url: str, retries: int = 5):
-    if not is_safe_url(url):
+@app.post("/v1")
+async def get_solverr(request: V1RequestBase):
+    if not is_safe_url(request.url):
         raise HTTPException(status_code=400, detail="Invalid URL")
     try:
-        driver = bypass_cloudflare(url, retries, log)
-        html = driver.html
-        cookies_json = json.dumps(driver.cookies(as_dict=True))
+        if request.cmd == "request.get":
+            driver = bypass_cloudflare(request.url, 5, log)
+            res = driver.listen.wait()
+            cookies = driver.cookies(as_dict=False)
+            
+            sol = ChallengeResolutionResultT()
+            sol.url = res.response.url
+            sol.headers = dict(res.response.headers.lower_items())
+            sol.status = res.response.status
+            sol.response = driver.html
+            sol.userAgent = driver.user_agent
+            sol.cookies = cookies
 
-        response = Response(content=html, media_type="text/html")
-        response.headers["cookies"] = cookies_json
-        response.headers["user_agent"] = driver.user_agent
-        driver.quit()
-        return response
+            driver.quit()
+
+            res = V1ResponseBase()
+            res.status = "ok"
+            res.solution = sol
+
+            return res
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-
 
 # Main entry point
 if __name__ == "__main__":
@@ -129,11 +125,6 @@ if __name__ == "__main__":
     parser.add_argument("--headless", action="store_true", help="Run in headless mode")
 
     args = parser.parse_args()
-    if args.headless and not DOCKER_MODE:
-        from pyvirtualdisplay import Display
-
-        display = Display(visible=0, size=(1920, 1080))
-        display.start()
     if args.nolog:
         log = False
     else:
